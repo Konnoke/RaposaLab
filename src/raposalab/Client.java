a7c4902afc089b1bfcdfde0fac0f962096150f39
Kevin Baik <Konnoke@live.com>
2017-03-09 11:07:12 -0500

    17.1-17.2

---
+++ src/raposalab/Client.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raposalab;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Zarathustra
+ */
+public class Client extends Thread {
+
+  private int port;
+  private String host;
+
+  public Client(String host, int port) throws IOException {
+    this.host = host;
+    this.port = port;
+  }
+
+  public static void main(String[] args) {
+    //int port = Integer.parseInt(args[1]);
+    try {
+      //Thread t = new Client("localhost", 5001);
+      Thread a = new Client("localhost", 65419);
+
+      a.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void run() {
+    try {
+      Socket socket = new Socket(host, port);
+
+      DataInputStream in = new DataInputStream(socket.getInputStream());
+      BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+      DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+      while (true) {
+        System.out.print("Enter response: ");
+        String response = console.readLine();
+        out.writeUTF(response);
+
+        String message = in.readUTF();
+        System.out.println(message);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ src/raposalab/Server.java
@@ -0,0 +1,52 @@
+package raposalab;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Zarathustra aka Kevin Baik
+ */
+public class Server extends Thread {
+
+  private ServerSocket serverSocket;
+
+  public Server(int port) throws IOException {
+    serverSocket = new ServerSocket(port);
+  }
+
+  public void run() {
+    try {
+      Socket client = serverSocket.accept();
+
+      DataInputStream in = new DataInputStream(client.getInputStream());
+      BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+      DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+      while (true) {
+        String message = in.readUTF();
+        System.out.println(message);
+        System.out.print("Enter response: ");
+        String response = console.readLine();
+
+        out.writeUTF(response);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    //int port = Integer.parseInt(args[0]);
+
+    try {
+      Thread t = new Server(5001);
+      
+      
+      t.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
